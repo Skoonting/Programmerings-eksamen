fec308cbf9b6447ec595b3f0f99d68229478f2e0
Silenzed <34092909+Silenzed@users.noreply.github.com>
2017-12-08 18:34:02 +0100

    Add files via upload

--- validerbilde.js
+++ validerbilde.js
@@ -1,50 +1,57 @@
-﻿function validerBilde(bilde)
+function fjernMelding()
+
 {
+   document.getElementById("melding").innerHTML="";
+}
 
+function validerbilde(bilde)
+{
 var tegn1, tegn2, tegn3;
-var lovligBilde=true;
+var lovligbilde=true;
 
-if (!bilde)  /* feltet for bilde er ikke bylt ut */
+if (!bilde)
   {
-    lovligKlassekode=false;
+    lovligbilde=false;
   }
-else if (bilde.length!=3)  /* bilde består ikke av 3 tegn */
+else if (bilde.length!=3)
   {
-    lovligBilde=false;
+    lovligbilde=false;
   }
 else
   {
-    tegn1=bilde.substr(0,1);  /* første tegn i bilde */
-    tegn2=bilde.substr(1,1);  /* andre tegn i bilde */
-    tegn3=bilde.substr(2,1);  /* tredje tegn i bilde */
+    tegn1=bilde.substr(0,1);
+    tegn2=bilde.substr(1,1);
+    tegn3=bilde.substr(2,1);
 
-    if (tegn1 < "0" || tegn1 > "9" || tegn2 < "0" || tegn2 > "9" || tegn3 < "1" || tegn3 > "9")  /* minst ett av tegnene er ulovlig */
+    if (tegn1 < "0" || tegn1 > "9" || tegn2 < "0" || tegn2 > "9" || tegn3 < "1" || tegn3 > "9") 
       {
-        lovligBilde=false;
+        lovligbilde=false;
       }
   }
 
-  return lovligBilde;
+  return lovligbilde;
 }
 
 function validerRegistrerBilde()
 {
+var bilde=document.getElementById("bilde").value;
 
-  var bilde=document.getElementById("bilde").value;
-
-  var lovligBilde=validerBilde(bilde);  /* valideringsfunksjon utført */
+  var lovligbilde=validerbilde(bilde);
 
   var feilmelding="";
-
-  if (!lovligBilde)  /* bilde er ikke korrekt fylt ut */
+if (!lovligbilde)
     {
-        feilmelding=feilmelding + "Bilde er ikke korrekt fylt ut <br />";
+        feilmelding=feilmelding + "bilde er ikke korrekt fylt ut <br />";
     }
 
-  else
+  if (lovligbilde)
     {
+        return true;
+    }
+    else
+      {
         document.getElementById("melding").style.color="red";
         document.getElementById("melding").innerHTML=feilmelding;
-        return false;
-    }
-}
+          return false;
+      }
+  }
