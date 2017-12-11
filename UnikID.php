795414c794397d9065b331485b03b1fde3694963
makmarian <bmartima@gmail.com>
2017-12-11 02:55:12 +0100

    123
    123

--- UnikID.php
+++ UnikID.php
@@ -1,29 +1,25 @@
 <?php
 $filnavn1 = "bildetest/bilde.txt"; 
-
 $filoperasjon1="r";
-
 $fil1 = fopen($filnavn1, $filoperasjon1); 
-
 $eksisterendeBilderArray = array(); //Lager en array som skal ha informasjon i seg hvis det er noe
-
 while($linje1 = fgets ($fil1)){
     if($linje1 != ""){
         $del1 = explode(";", $linje1);
-        $kKode1 = trim($del1[0]); //kKode1 er midlertidlig variabel, skal endre det etterpå
-
+        $kKode1 = trim($del1[0]);
         if($kKode1 == $bildeNr){
-            array_push($eksisterendeBilderArray, "hei"); //Jeg valgte bare et random ord
+            array_push($eksisterendeBilderArray, "a"); 
             /*
-            Hvis koden man skriver inn i feltet allerede finnes vil den skrive ut et radnom or eller krakter,
+                array_push, legger verdier i en array.
+                Ved bruk av dette i en en while-loop, legger den til en verdi i arrayen hvis den ser at man har skrevet inn en kode som allerede er i bilde.txt. 
+                Hvis det er noen verdi i arrayen, får man en feilmelding om at koden er allerede registert. 
             */
         } 
     }
 }
-
 if($eksisterendeBilderArray){ //Hvis det er noen ting i arrayen betyr det at det den finnes fra før av i koden, derfor blir den false.
-    $qqq = false; //Bare et tilfeldig varable navn, skal endre det etterpå en gang.
-    print("Denne koden finnes allerede<br>"); 
+    $qqq = false;
+    print("Denne koden finnes allerede<br>");
 } else { //Hvis den er tom betyr det at den ikke fant noen som hadde samme navnet
     $qqq = true;
 }
