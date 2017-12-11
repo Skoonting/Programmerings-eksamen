45e3087ea1aa37c69eab228bda9b316d9ec3978e
makmarian <bmartima@gmail.com>
2017-12-11 02:50:58 +0100

    lagt til include
    dwddwd

--- bilde.php
+++ bilde.php
@@ -1,102 +1,93 @@
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <title>Bilde</title> 
-  </head>
-<body>
-          <form method="post" action="" id="registrer-bilde" name="registrer-bilde" onsubmit="return validateForm()">
-              <h3>Bilde</h3>
-              <label for="bildeNr"><p>Bildenummer:</p></label><span id="bildeNrOut"></span>
-              <input required name="bildeNr"  id="bildeNr">
-              
-              <label for="opDato"><p>Opplastingsdato:</p></label><span id="opDatoOut"></span>
-              <input required name="opDato" id="opDato"><br>
-              
-              <label for="filNavn"><p>Filnavn:</p></label><span id="filNavnOut"></span>
-              <input required name="filNavn"  id="filNavn">
-              
-              <label for="beskrivelse"><p>Beskrivelse:</p></label><span id="beskrivelseOut"></span>
-              <input required name="beskrivelse" id="beskrivelse"><br>
+<?php include("start.html") ?>
+    
+<form method="post" action="" id="registrer-bilde" name="registrer-bilde" onsubmit="return validateForm()">
+  <h3>Bilde</h3>
+  <label for="bildeNr"><p>Bildenummer:</p></label>
+  <input required name="bildeNr" id="bildenr" onFocus="fokus(this)" onBlur="mistetFokus(this)" onMouseOver="musInn(this)" onMouseOut="musUt()" onChange="endretilstorebokstaver(this)">
 
-              <input  class="inputClass" type="submit" value="Registrer" id="registrer" name="registrer">
-              <input  class="inputClass" type="reset" value="Nullstill" id="nullstill" name="nullstill">
-              
-              <span id="jsOutput"></span>
-            </form> 
-            
-            <script><?php include("bildeJS-validering.js"); ?></script>
-          
-            <?php
-            if(isset($_POST['registrer'])){
-                
-                $filnavn     = "bildetest/bilde.txt";
+  <label for="opDato"><p>Opplastingsdato:</p></label>
+  <input required name="opDato" id="opDato" onFocus="fokus(this)" onBlur="mistetFokus(this)" onMouseOver="musInn(this)" onMouseOut="musUt()" onChange="endretilstorebokstaver(this)"><br>
 
-                $bildeNr     = $_POST["bildeNr"];
-                $opDato      = $_POST["opDato"];
-                $filNavn     = $_POST["filNavn"];
-                $beskrivelse = $_POST["beskrivelse"];
+  <label for="filNavn"><p>Filnavn:</p></label>
+  <input required name="filNavn"  id="filNavn" onFocus="fokus(this)" onBlur="mistetFokus(this)" onMouseOver="musInn(this)" onMouseOut="musUt()" onChange="endretilstorebokstaver(this)">
 
-                $bildeNr     = trim($bildeNr);
-                $opDato      = trim($opDato);
-                $filNavn     = trim($filNavn);
-                $beskrivelse = trim($beskrivelse);
-                
-                $opDato      = mb_strtoupper($opDato, 'UTF-8');
-                $filNavn     = mb_strtoupper($filNavn, 'UTF-8');
-                $beskrivelse = mb_strtoupper($beskrivelse, 'UTF-8');
+  <label for="beskrivelse"><p>Beskrivelse:</p></label>
+  <input required name="beskrivelse" id="beskrivelse" onFocus="fokus(this)" onBlur="mistetFokus(this)" onMouseOver="musInn(this)" onMouseOut="musUt()" onChange="endretilstorebokstaver(this)"><br>
 
-                if(!$bildeNr){
-                    print("Bildenummer mangeler <br>");
-                    $bool1 = false;
-                } else {
-                    $bool1 = true;
-                }
-                
-                if(!$opDato){
-                    print("Opplastingsdato mangeler <br>");
-                    $bool2 = false;
-                } else {
-                    $bool2 = true;
-                }
-                
-                if(!$filNavn){
-                    print("Filnavn mangeler <br>");
-                    $bool3 = false;
-                } else {
-                    $bool3 = true;
-                }
-                
-                if(!$beskrivelse){
-                    print("Beskrivelse mangeler <br>");
-                    $bool4 = false;
-                } else {
-                    $bool4 = true;
-                }
-                
-                /*----------------------------------------------------------*/
-                
-                
-                
-                include("unikBildeID.php");
-                
-                /*----------------------------------------------------------*/
-                
-                    if($bool1 == true && $bool2 == true && $bool3 == true && $bool4 == true && $qqq == true) {
-                    
-                    $filoperasjon = "a";
+  <input  class="inputClass" type="submit" value="Registrer" id="registrer" name="registrer">
+  <input  class="inputClass" type="reset" value="Nullstill" id="nullstill" name="nullstill">
+    
+</form> 
 
-                    $linje = $bildeNr . ";" . $opDato . ";" . $filNavn . ";" . $beskrivelse . "\n";
+  <div id="melding"></div>
+  
+  <div id="jsOutput"></div>
 
-                    $fil = fopen($filnavn, $filoperasjon); 
+<script><?php include("bildeJS-validering.js") ?></script>
 
-                    fwrite($fil,$linje);
+<?php
+if(isset($_POST['registrer'])){
 
-                    echo("$bildeNr $opDato $filNavn $beskrivelse");
-                    } else {
-                        print("Dette funker ikke");
-                    }                
-            }
-            ?>       
-</body>
-</html>
+    $filnavn     = "bildetest/bilde.txt"; 
+
+    $bildeNr     = $_POST["bildeNr"];
+    $opDato      = $_POST["opDato"];
+    $filNavn     = $_POST["filNavn"];
+    $beskrivelse = $_POST["beskrivelse"];
+
+    $bildeNr     = trim($bildeNr);
+    $opDato      = trim($opDato);
+    $filNavn     = trim($filNavn);
+    $beskrivelse = trim($beskrivelse);
+
+    $filNavn     = mb_strtoupper($filNavn, 'UTF-8'); //Bruker mb_strtoupper for å gjøre teksten til store bokstaver før den blir lagret (mb_strtoupper i stedet for strtoupper siden den tar også med æøå)
+    $beskrivelse = mb_strtoupper($beskrivelse, 'UTF-8');
+
+    if(!$bildeNr){
+        print("Bildenummer mangeler <br>");
+        $bool1 = false;
+    } else {
+        $bool1 = true;
+    }
+
+    if(!$opDato){
+        print("Opplastingsdato mangeler <br>");
+        $bool2 = false;
+    } else {
+        $bool2 = true;
+    }
+
+    if(!$filNavn){
+        print("Filnavn mangeler <br>");
+        $bool3 = false;
+    } else {
+        $bool3 = true;
+    }
+
+    if(!$beskrivelse){
+        print("Beskrivelse mangeler <br>");
+        $bool4 = false;
+    } else {
+        $bool4 = true;
+    }
+
+    /*---------------------------*/
+    include("unikBildeID.php");
+    /*---------------------------*/
+
+        if($bool1 && $bool2 && $bool3 && $bool4 && $qqq) {
+
+        $filoperasjon = "a";
+
+        $linje = $bildeNr . ";" . $opDato . ";" . $filNavn . ";" . $beskrivelse . "\n";
+
+        $fil = fopen($filnavn, $filoperasjon); 
+
+        fwrite($fil,$linje);
+
+        echo("$bildeNr $opDato $filNavn $beskrivelse");
+        }                
+}
+?>       
+
+<?php include("slutt.html") ?>
