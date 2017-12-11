b58b395cabe23ecee3870377b7685110a3f32be7
makmarian <bmartima@gmail.com>
2017-12-11 02:53:24 +0100

    123
    123

--- bilde-vis.php
+++ bilde-vis.php
@@ -1,30 +1,26 @@
-<!DOCTYPE html>
-<html lang="no">
-  <head>
-    <meta charset="UTF-8">
-    <title>Bilde-vis</title>
-  </head>
-  <body>
-	   <h3>Vis Bilder</h3>
-        <?php
-        $filnavn = "bildetest/bilde.txt";
+<?php include("start.html") ?>
 
-        $filoperasjon="r";
+<h3>Vis Bilder</h3>
 
-        $fil = fopen($filnavn, $filoperasjon);
+<?php
+    $filnavn = "bildetest/bilde.txt";
 
-        while($linje = fgets ($fil)){
-            if($linje != ""){
-                $del = explode(";", $linje);
-                $bildeNr     = trim($del[0]);
-                $opDato      = trim($del[1]);
-                $filNavn     = trim($del[2]);
-                $beskrivelse = trim($del[3]);
+    $filoperasjon="r";
 
-                print("$bildeNr $opDato $filNavn $beskrivelse <br>");
-            }
+    $fil = fopen($filnavn, $filoperasjon);
+
+    while($linje = fgets ($fil)){
+        if($linje != ""){
+            $del = explode(";", $linje);
+            $bildeNr     = trim($del[0]);
+            $opDato      = trim($del[1]);
+            $filNavn     = trim($del[2]);
+            $beskrivelse = trim($del[3]);
+
+            print("$bildeNr $opDato $filNavn $beskrivelse <br>");
         }
-        fclose($fil);
-        ?>
-    </body>
-</html>
+    }
+    fclose($fil);
+?>
+
+<?php include("slutt.html") ?>
