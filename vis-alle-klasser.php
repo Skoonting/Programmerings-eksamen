7f507e7e1cae2897220f97ce2d1b2922ce64187b
makmarian <bmartima@gmail.com>
2017-12-12 16:00:20 +0100

    Lettere å lese nå

--- vis-alle-klasser.php
+++ vis-alle-klasser.php
@@ -2,94 +2,48 @@
 <html lang="no">
   <head>
     <meta charset="UTF-8">
-    <title>Vis alle klasser</title>  
-      <link rel="icon" href="icon.png">
-  <link rel="stylesheet" type="text/css" href="styles.css">
-      <script>
-        
-      </script>
   </head>
   <body>
-    <div class="container">
-      
-      <header><h2>Obligatorisk oppgave 2</h2></header>
-      
-      <nav>
-          <ul>
-            <li class="titleT" id="tittelId"><strong>Meny</strong></li>
-            <li><a href="index.php">Hjem</a></li>
-            <li class="titleT"><strong>Klasse</strong></li>
-            <li><a href="registrer-klasse.php">Registrer klasse</a></li>
-            <li class="active"><a href="#">Vis alle klasser</a></li>
-            <li class="titleT"><strong>Student</strong></li>
-            <li><a href="registrer-student.php">Registrer student</a></li>
-            <li><a href="vis-alle-studenter.php">Vis alle studenter</a></li>
-            <li><a href="vis-klasseliste.php">Vis klasseliste</a></li>
-          </ul>
-      </nav>
-        
-      <main>
-	   <h3>Vis alle klasser</h3>
-        <?php
-        $filnavn = "klasse.txt";
+   <h3>Vis alle klasser</h3>
+    
+    <?php
+    $filnavn = "klasse.txt";
 
-        $filoperasjon="r";
+    $filoperasjon="r";
 
-        $fil = fopen($filnavn, $filoperasjon); 
+    $fil = fopen($filnavn, $filoperasjon); 
 
-        echo("<table>");
-        echo("<tr><th>Klassekode</th><th>Klassenavn</th></tr>");
+    $a = array(); //Denne arrayen inneholder alle de andre under-arrayene
 
-          $a = array();
-          
-        while($linje = fgets ($fil)){
-            if($linje != ""){
-                $del = explode(";", $linje);
-                $kKode = trim($del[0]);
-                $kNavn = trim($del[1]);
+    while($linje = fgets ($fil)){
+        if($linje != ""){
+            $del = explode(";", $linje);
+            $kKode = trim($del[0]);
+            $kNavn = trim($del[1]);
 
-                array_push($a, array(($kKode), array($kNavn, $kKode)));
-            }
+            array_push($a, array(($kKode), array($kNavn, $kKode, $kNavn)));
+            /*
+            array_push($a, array((verdien man vill sortere på), array(verdier i arrayen)));
+            */
         }
-        
-                /*
-                $mat = array(
-                    array("Jens", array('verdi1', 'burger', 'ost')),
-                    array("Kåre", array('verdi2', 'kjott', 'salat')),
-                    array("Magnus", array('verdi3', 'kjott', 'salat')),
-                    array("Arne", array('verdi4', 'kjott', 'salat'))
-                );
-          
-                sort($mat);
-                sort($a);
-                                
-                $lengde = count($mat);
-                for($i = 0; $i < $lengde; $i++){
-                    print("Person: " . $mat[$i][0] . " verdi: " . $mat[$i][1][0]);   
-                }
-                */
-                sort($a);
-          
-                $lengdeJ = count($a);
-                
-                
-                          
-                for($j = 0; $j < $lengdeJ; $j++){
-                    $lengdeK = count($a[$j][1]);
-                    for($k = 0; $k < $lengdeK; $k++){
-                        
-                    }
-                    print("Yrke: " . $a[$j][0] . "<br>");   
-                }
-                    
-        echo("</table>");
-        fclose($fil);
-        ?>
-          <script>
-            
-          </script>
-        </main>
-        <footer><p>&copy; Bjørn Martin</p></footer>
-    </div>
+    }
+
+    sort($a); //sorterer arrayen alfabetisk
+
+    $lengdeJ = count($a); //Finner ut hvor mange arrayer/verdier det er i arrayen
+
+    $testStreng = ""; //Denne verdien lagrer informasjonen som en streng, som senere skal bli skrevet ut
+
+    for($j = 0; $j < $lengdeJ; $j++){ //første for-loop går igjennom hoved-arrayen
+        $lengdeK = count($a[$j][1]); //sjekker hvor mange verdier det er inni hver under-array
+        for($k = 0; $k < $lengdeK; $k++){ //går igjennom under-arrayen
+            $testStreng .= $a[$j][1][$k]; //legger til alle underverdiene til underarrayen
+        }
+        print("Yrke: " . $a[$j][0] . " Person: " . $testStreng . "<br>"); //Første er yrket, det andre er verdien til denne yrken
+        $testStreng = ""; //sletter det som er i strengen, eller så vil alt bli skrivd opp på nytt
+    }
+
+    fclose($fil);
+    ?>
   </body>
 </html>
